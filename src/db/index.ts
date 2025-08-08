ecfdcfc66bc62faecb392bed41ff239272d52062
andersonkaiti <anderkaiti@gmail.com>
2025-08-08 09:36:51 -0300

    :wrench: chore(database): add better auth tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,27 @@
 import { env } from '@config/env'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import {
+  accountTable,
+  categoryRelations,
+  categoryTable,
+  productRelations,
+  productTable,
+  productVariantRelations,
+  productVariantTable,
+  sessionTable,
+  userTable,
+} from './schema'
 
-export const db = drizzle(env.DATABASE_URL)
+export const db = drizzle(env.DATABASE_URL, {
+  schema: {
+    accountTable,
+    categoryRelations,
+    categoryTable,
+    productRelations,
+    productTable,
+    productVariantRelations,
+    productVariantTable,
+    sessionTable,
+    userTable,
+  },
+})
